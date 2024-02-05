76b739121484e0c87aa3dabfb825e27aa58a839a
manuellaurino <158981853+manuellaurino@users.noreply.github.com>
2024-02-05 00:18:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+function reproducirVideo(videoId) {
+  switch (videoId) {
+    case 'video1':
+      window.location.href = 'url_del_video_1.mp4';
+      break;
+    case 'video2':
+      window.location.href = 'url_del_video_2.mp4';
+      break;
+    case 'video3':
+      window.location.href = 'url_del_video_3.mp4';
+      break;
+    case 'video4':
+      window.location.href = 'url_del_video_4.mp4';
+      break;
+    case 'video5':
+      window.location.href = 'url_del_video_5.mp4';
+      break;
+    case 'video6':
+      window.location.href = 'url_del_video_6.mp4';
+      break;
+    case 'video7':
+      window.location.href = 'url_del_video_7.mp4';
+      break;
+    case 'video8':
+      window.location.href = 'url_del_video_8.mp4';
+      break;
+    case 'video9':
+      window.location.href = 'url_del_video_9.mp4';
+      break;
+    case 'video10':
+      window.location.href = 'url_del_video_10.mp4';
+      break;
+    case 'video11':
+      window.location.href = 'url_del_video_11.mp4';
+      break;
+    case 'video12':
+      window.location.href = 'url_del_video_12.mp4';
+      break;
+    case 'video13':
+      window.location.href = 'url_del_video_13.mp4';
+      break;
+    case 'video14':
+      window.location.href = 'url_del_video_14.mp4';
+      break;
+    case 'video15':
+      window.location.href = 'url_del_video_15.mp4';
+      break;
+    default:
+      console.error('ID de video no válido');
+  }
+}
